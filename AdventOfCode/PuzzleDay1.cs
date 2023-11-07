f5ff08b5d1bb1e35b04e214134adf86ec9777588
Henrik Maaland <henrikmaa@gmail.com>
2023-11-07 19:56:05 +0100

    Solve day 1

--- AdventOfCode/PuzzleDay1.cs
+++ AdventOfCode/PuzzleDay1.cs
@@ -8,14 +8,59 @@ public class PuzzleDay1 : Puzzle
     public override string Part1()
     {
 
-        var input = GetInput(); //This is the input of the current day
-        return "TODO: Solve puzzle Part1";
+        var sums = GetCaloriesAmounts();
+        
+        var mostCalories = sums.OrderByDescending(n => n).Take(1).ToList();
+        
+        return $"{mostCalories[0]}";
     }
 
     public override string Part2()
     {
-        var input = GetInput(); //This is the input of the current day
-        return "TODO: Solve puzzle Part2";
+        var sums = GetCaloriesAmounts();
+        
+        var topThree = sums.OrderByDescending(n => n).Take(3).ToList();
+
+        var topThreeTotal = topThree[0] + topThree[1] + topThree[2];
+        return $"{topThreeTotal}";
+    }
+
+    public List<int> GetCaloriesAmounts()
+    {
+        
+
+        var input = GetInput();
+        
+        string[] lines = input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+        List<int> sums = new List<int>();
+        int currentSum = 0;
+
+        foreach (string line in lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                if (currentSum > 0)
+                {
+                    sums.Add(currentSum);
+                    currentSum = 0;
+                }
+            }
+            else
+            {
+                if (int.TryParse(line, out int number))
+                {
+                    currentSum += number;
+                }
+            }
+        }
+        
+        if (currentSum > 0)
+        {
+            sums.Add(currentSum);
+        }
+
+        return sums;
     }
 }
 
